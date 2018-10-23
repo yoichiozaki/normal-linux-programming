4e0abe443766891c4ae19ab31437169b2cff9d98
zakimal <oza777you@gmail.com>
2018-10-23 14:55:03 +0900

    dev head

--- head.c
+++ head.c
@@ -1,16 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define _GUN_SOURCE
+#include <getopt.h>
 
 static void do_head(FILE *f, long nlines);
+
+#define DEFAULT_N_LINES 10
+
+static struct option longops[] = {
+    {"lines", required_argument, NULL, 'n'},
+    {"help", no_argument, Null, 'h'},
+    {0, 0, 0, 0}
+};
 
 int
 main(int argc, char *argv[])
 {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s n\n", argv[0]);
-        exit(1);
+    int opt;
+    long nlines = DEFAULT_N_LINES;
+
+    while ((opt = getopt_long(argc, argv, "n:", longopts, NULL)) != -1) {
+        switch (opt) {
+            case 'n':
+                nlines = atol(optarg);
+                break;
+            case 'h':
+                fprintf(stdout, "Usage: %s [-n LINES] [FILE ...]\n", argv[0]);
+                exit(0);
+            case '?':
+                fprintf(stderr, "Usage: %s [-n LINES] [FILE ...]\n", argv[0]);
+                exit(1);
+        }
     }
-    do_head(stdin, atol(argv[1]));
+
+    if (optind == argc) {
+        do_head(stdin, nlines);
+    } else {
+        int i;
+        for (i = optind; i < argc; i++) {
+            FILE *f;
+            f = fopen(argv[i], "r");
+            if (!f) {
+                perror(argv[i]);
+                exit(1);
+            }
+            do_head(f, nlines);
+            fclose(f);
+        }
+    }
     exit(0);
 }
 
